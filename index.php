bed2be7e6810a6d269ab46b4bfb2ff808e3a5076
Slizovskaya <maria.slizovskaya@gmail.com>
2020-04-14 14:34:36 +0300

    first option of calc with a long check

--- index.php
+++ index.php
@@ -4,8 +4,42 @@
     <h1>
         Calculator
     </h1>
-    <h2>
-       Amount:<?php echo $_GET['a'] + $_GET['b']; ?>
-    </h2>           
+    <p>
+    <?php
+       
+        $operation = $_GET['o'];
+        $a = $_GET['a'];
+        $b = $_GET['b'];
+
+        if ($operation != 'sum' && $operation != 'sub' && $operation != 'div' && $operation != 'mul') {
+            echo 'Unexpected operation';
+            exit;
+        }
+
+        if (!is_numeric($a) || !is_numeric($b)) {
+           echo 'Invalid arguments';
+           exit;
+        }
+
+        if ($operation == 'sum') {
+            $result = $a + $b;
+        }
+
+        if ($operation == 'sub') {
+            $result = $a - $b;
+        }
+
+        if ($operation == 'div') {
+            $result = $a / $b;
+        }
+
+        if ($operation == 'mul') {
+            $result = $a * $b;
+        }
+
+       
+        echo $operation, ' is ', $result;
+    ?>
+    </p>
 </body>
 </html>
