6bb78594885e8bbb22de03a9348253ffd5fcb203
Slizovskaya <maria.slizovskaya@gmail.com>
2020-04-27 01:31:01 +0300

    Calc with POST

--- index.php
+++ index.php
@@ -1,35 +1,57 @@
 <html>
 <head></head>
 <body>
-    <h1>
-        Calculator
-    </h1>
-    <p>
-    <?php
-       
-        $operation = $_GET['o'];
-        $a = $_GET['a'];
-        $b = $_GET['b'];
-        $result = null;
+  <h1>Calculator</h1>
+  <?php
+    $a = (int) $_POST['a'];
+    $b = (int) $_POST['b'];
+    $operation = isset($_POST['operation']) ? $_POST['operation'] : 'undefined';
+  ?>
+  <form method="POST">
+    <div>
+       <label>A</label>
+       <input type="text" name="a" placeholder="Input a here">                                                                                                        
+    </div>
+    <div>
+        <label>B</label>
+        <input type="text" name="b" placeholder="Input b here">
+    </div>
+    <div>
+        <label>Operation</label>
+        <select name="operation">
+            <option value="Sum">Sum</option>
+            <option value="Sub">Subtraction</option>
+            <option value="Mul">Multiply</option>
+            <option value="Div">Divide</option>
+        </select>
+    </div>
+    <br>
+    <div>
+        <button type="submit">Calculate</button>
+    </div>
+  </form>
+   <p><h2>
+      <?php       
+         $result = null;
 
         if (!is_numeric($a) || !is_numeric($b)) {
            echo 'Invalid arguments';
            exit;
         }
 
-        if ($operation === 'sum') {
+        if ($operation === 'Sum') {
             $result = $a + $b;
         }
 
-        if ($operation === 'sub') {
+        if ($operation === 'Sub') {
             $result = $a - $b;
         }
 
-        if ($operation === 'div') {
+        if ($operation === 'Div') {
             $result = $a / $b;
         }
 
-        if ($operation === 'mul') {
+        if ($operation === 'Mul') {
             $result = $a * $b;
         }
 
@@ -38,8 +60,8 @@
             exit;
         }
        
-        echo $operation, ' is ', $result;
-    ?>
-    </p>
+        echo 'Result:', $result;
+    ?>  
+  </p></h2>
 </body>
 </html>
